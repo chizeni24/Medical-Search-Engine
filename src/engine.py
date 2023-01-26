80abc07c7ef8a8fd104f9f6014a1d61728defcd5
chizeni24 <zvy431@my.utsa.edu>
2023-01-25 19:30:53 -0600

    Update the requirement file

--- src/engine.py
+++ src/engine.py
@@ -7,9 +7,15 @@ from ML_pipeline.top_n import top_n
 import pandas as pd
 from gensim.models import Word2Vec
 
+
+
+import os
+
+current_dir = os.getcwd()
+parent_dir = os.path.dirname(current_dir)
 ### Read the initial dataset ###
-df = read_data("../input/Dimension-covid.csv")
-df1 = read_data("../input/Dimension-covid.csv")  
+df = read_data(os.path.join(parent_dir,"input/Dimension-covid.csv"))
+df1=read_data(os.path.join(parent_dir, "input/Dimension-covid.csv"))
 
 ### Pre-processing the data  ###
 x  = output_text(df,"Abstract")
@@ -18,33 +24,38 @@ x  = output_text(df,"Abstract")
 skipgram = model_train(df,"Abstract","Skipgram",vector_size = 100,window_size = 1)
 fasttext = model_train(df,"Abstract","Fasttext",vector_size=100, window_size = 2)
 
+# change the directory to upper level 
+os.chdir('..')
 ### Loading our pretrained model ###
-skipgram = Word2Vec.load('../output/model_Skipgram.bin')
-FastText = Word2Vec.load('../output/model_Fasttext.bin')
+skipgram = Word2Vec.load(os.path.join(os.getcwd(),'output/model_Skipgram.bin'))
+FastText = Word2Vec.load(os.path.join(os.getcwd(),'output/model_Fasttext.bin'))
 
 ### convert to columns vectors using skipgram ###
 K1_abstract = return_embed(skipgram,df,"Abstract")
 K1_abstract = pd.DataFrame(K1_abstract).transpose()    # Saving vectors of each abstract in data frame so that we can use directly while running code again
-K1_abstract.to_csv('../output/skipgram-vec-abstract.csv')
+K1_abstract.to_csv(os.path.join(os.getcwd(),'output/skipgram-vec-abstract.csv'))
 
 K1_title = return_embed(skipgram,df,"Title")
 K1_title = pd.DataFrame(K1_title).transpose()  
-K1_title.to_csv('../output/skipgram-vec-title.csv')
+K1_title.to_csv(os.path.join(os.getcwd(), 'output/skipgram-vec-title.csv'))
 
 ### convert to columns vectors using FastText ###
 K2_abstract = return_embed(FastText,df,"Abstract")
 K2_abstract = pd.DataFrame(K1_abstract).transpose()    
-K2_abstract.to_csv('../output/FastText-vec-abstract.csv')
+K2_abstract.to_csv(os.path.join(os.getcwd(),'output/FastText-vec-abstract.csv'))
 
 K2_title = return_embed(FastText,df,"Title")
 K2_title = pd.DataFrame(K1_title).transpose()    
-K2_title.to_csv('../output/FastText-vec-title.csv')
+K2_title.to_csv(os.path.join(os.getcwd(),'output/FastText-vec-title.csv'))
 
 ### Loading our pretrained vectors of each abstract ###
-K = read_data('../output/skipgram-vec-abstract.csv')   
+K = read_data(os.path.join(os.getcwd(),'output/skipgram-vec-abstract.csv'))   
 skipgram_vectors=[]                          #transforming dataframe into required array like structure as we did in above step
 for i in range(df.shape[0]):
     skipgram_vectors.append(K[str(i)].values)
 
+os.chdir('src')
 ### Function to return 'n' similar results  ### 
 Results = top_n('Coronavirus','Skipgram','Abstract')
+
+
